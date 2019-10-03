22b986827aecf722123ad9c80b51db0e464f22cf
Colleen <colleen@Colleens-MBP.domain>
2019-10-02 21:49:55 -0500

    scrapes properly

--- adam-viator-script.py
+++ adam-viator-script.py
@@ -1,11 +1,32 @@
-import requests
-import time
+# encoding=utf8
+import requests, re
 from bs4 import BeautifulSoup
 
 tours = []
 url = 'https://www.viator.com/Ireland/d56-ttd'
-r = requests.get(url)
-soup = BeautifulSoup(r.text, "html.parser")
-tours.append(soup.findAll('div', {'class', 'product-card'}))
+iter = True
+i = 1
+while (iter):
+    if (i == 1):
+        r = requests.get(url)
+    else:
+        print(url + '/' + str(i))
+        r = requests.get(url + '/' + str(i))
+    i += 1
 
-print(tours)
+    soup = BeautifulSoup(r.text, "html.parser")
+    #print (str(r.status_code) + ' - ' + str(i))
+    if (r.status_code != 404):
+        tours.append(soup.find_all(attrs={'data-action-tag':'view_product'}))
+    else:
+        iter = False
+
+print (len(tours))
+with open('index.html', 'w+') as f:
+    f.seek(0)
+    for tour in enumerate(tours):
+        for t in tour:
+            try:
+                f.write(BeautifulSoup((str(t)).encode('ascii', 'ignore').decode('ascii'), 'html.parser').prettify())
+            except Exception as e:
+                print(e)
